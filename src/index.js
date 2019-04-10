2442c1a48199be6ae11b15cecb0f20d516213056
Catherine Zhao <catherine.zhao.20@dartmouth.edu>
2019-04-10 17:07:54 -0400

    deployed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+  num += 1;
+}, 1000);
