6f210ca99d4da27f7b83774e891e002f9b400bbe
Catherine Zhao <catherine.zhao.20@dartmouth.edu>
2019-04-12 00:52:12 -0400

    sa4 start

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 // change require to es6 import style
 import $ from 'jquery';
+import React from 'react';
+// import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
+
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+// class App extends Component {
+//   render() {
+//     return (<div className="test">All the REACT are belong to us!</div>);
+//   }
+// }
+
+ReactDOM.render(<App />, document.getElementById('main'));
 
 let num = 0;
 setInterval(() => {
